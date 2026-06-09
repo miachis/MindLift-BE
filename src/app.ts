00313278003337e1201e8d611964ff865736257e
Miachi Solomon <smiachi10@gmail.com>
2026-06-09 15:08:05 +0100

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./src/.env" });
 import express from "express";
+import cors from "cors";
 
 import otpRouter from "./otp/otpRouter.js";
 import signupRouter from "./signup/signUpRouter.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/otp", otpRouter);
 app.use("/signup", signupRouter);
