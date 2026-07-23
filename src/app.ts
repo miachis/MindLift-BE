d232f4ce44f8d4306db90afeb5777058355c29a9
Miachi Solomon <smiachi10@gmail.com>
2026-07-23 14:44:53 +0100

    prisma config edit for prod#8

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-dotenv.config({ path: "./src/.env" });
+dotenv.config({ path: "./.env" });
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
